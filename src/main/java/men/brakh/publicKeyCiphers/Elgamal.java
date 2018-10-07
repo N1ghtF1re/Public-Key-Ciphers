ba8685d0d1d555c34e9639bb7e37f9714fa4564d
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-10-07 16:30:43 +0300

    Cipher class created

---
+++ src/main/java/men/brakh/publicKeyCiphers/Elgamal.java
@@ -0,0 +1,10 @@
+package men.brakh.publicKeyCiphers;
+
+public class Elgamal {
+    private long p;
+    private long x;
+    private long k;
+    public Elgamal(long p, long x, long k) {
+
+    }
+}
